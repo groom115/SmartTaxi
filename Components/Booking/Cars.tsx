d4c62d546b8da75193ace9e9caceb57996d98e0a
“Abhishek <abhishek.shyam@brightchamps.com>
2023-09-30 09:21:00 +0530

    distance

--- Components/Booking/Cars.tsx
+++ Components/Booking/Cars.tsx
@@ -1,32 +1,57 @@
 import CarsList from "@/data/CarsList";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
+import { DirectionDataContext } from "@/Context/DirectionDataContext";
 const Cars = () => {
   const [selectedCar, setSelectedCar] = useState<number>();
+  const { directionData, setDirectionData } = useContext(DirectionDataContext);
+
+  const getCost = (charges: any) => {
+    return (
+      charges *
+      directionData.routes[0].distance *
+      0.000621371192
+    ).toFixed(2);
+  };
+
   return (
     <div className="mt-3">
-      <h1 className="font-semibold">Select Cars</h1>
-      <div className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3 ">
-        {CarsList.map((Car, index) => (
+      <h2 className="font-medium text-[14px] ">Select Car</h2>
+      <div
+        className="grid 
+        grid-cols-3 
+        md:grid-cols-2
+        lg:grid-cols-3
+         "
+      >
+        {CarsList.map((item, index) => (
           <div
-            className={`m-2 p-2 border-[1px] rounded-md hover:border-orange-200 hover:border-[2px] cursor-pointer ${
-              selectedCar === index ? "border-orange-200 border-[2px]" : ""
-            }`}
             key={index}
-            onClick={() => {
-              setSelectedCar(index);
-            }}
+            className={`m-2
+                 p-2 border-[1px] rounded-md 
+                 hover:border-yellow-400 
+                 cursor-pointer 
+                 ${
+                   index == selectedCar
+                     ? "border-yellow-400 border-[2px]"
+                     : null
+                 }`}
+            onClick={() => setSelectedCar(index)}
           >
             <Image
-              src={Car.image}
-              alt="cars"
+              src={item.image}
+              alt={item.name}
               width={75}
               height={90}
               className="w-full"
             />
-            <div className="flex justify-between mt-1">
-              <span className="text-[12px] text-gray-500">{Car.name}</span>
-              <span className="text-[12px] font-semibold">{Car.charges} $</span>
+            <div className="flex justify-around mt-1 lg:flex-col flex-col md:flex-row">
+              <span className="text-[12px] text-gray-500">{item.name}</span>
+              {directionData?.routes ? (
+                <span className="text-[12px] font-medium">
+                  {getCost(item.charges)}$
+                </span>
+              ) : null}
             </div>
           </div>
         ))}
