bcff43fe65d122ba792f81242e56d58b8ed9b10c
“Abhishek <abhishek.shyam@brightchamps.com>
2023-10-05 10:13:08 +0530

    commit

--- app/Payment/page.tsx
+++ app/Payment/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "@/Components/Payment/CheckoutForm";
+import Navbar from "@/Components/Navbar";
 
 const Payment = () => {
   const stripePromise = loadStripe(
@@ -13,11 +14,15 @@ const Payment = () => {
     mode: "payment",
     amount: 300,
     currency: "usd",
+    clientSecret: process.env.STRIPE_SECRET_KEY,
   };
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements>
+    <div>
+      <Navbar />
+      <Elements stripe={stripePromise} options={options}>
+        <CheckoutForm />
+      </Elements>
+    </div>
   );
 };
 
